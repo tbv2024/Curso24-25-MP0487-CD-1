b251997e74d2b291b711f9d42cc4330c37187ded
TaniaBarrous <taniabarvil@iesrodeira.gal>
2025-06-07 15:42:02 +0200

    este es un cambio en math

--- UNIDAD-01/CODIGO-PRUEBAS/Java/Ejemplos01/exercise06_03/MathTest.java
+++ UNIDAD-01/CODIGO-PRUEBAS/Java/Ejemplos01/exercise06_03/MathTest.java
@@ -4,9 +4,9 @@ public class MathTest {
    public static void main(String[] args) {
       System.out.printf("Math.abs(23.7) = %f%n", Math.abs(23.7));
       System.out.printf("Math.abs(0.0) = %f%n", Math.abs(0.0));
-      System.out.printf("Math.abs(-23.7) = %f%n", Math.abs(-23.7));
+      System.out.printf("Math.abs(-23.7) = %f%n", Math.abs(-11.7));
       System.out.printf("Math.ceil(9.2) = %f%n", Math.ceil(9.2));
-      System.out.printf("Math.ceil(-9.8) = %f%n", Math.ceil(-9.8));
+      System.out.printf("Math.ceil(-9.8) = %f%n", Math.ceil(6));
       System.out.printf("Math.cos(0.0) = %f%n", Math.cos(0.0));
       System.out.printf("Math.exp(1.0) = %f%n", Math.exp(1.0));
       System.out.printf("Math.exp(2.0) = %f%n", Math.exp(2.0));
